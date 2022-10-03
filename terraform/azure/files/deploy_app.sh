30d4851360b8ec7ca8b914a6bce3684edb9231ee
AuditClay <42394776+AuditClay@users.noreply.github.com>
2022-10-03 10:22:40 -0500

    fix deploy script

--- terraform/azure/files/deploy_app.sh
+++ terraform/azure/files/deploy_app.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
-
-echo "Copying index.html"
 
 cat << EOM > /var/www/html/index.html
 <html>
@@ -11,9 +9,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://placekitten.com/800/600"></img></center>
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to the Meow World App! 
+  Welcome to ${PREFIX}'s app. Replace this text with your own. 
   <!-- END -->
   
   </div>
